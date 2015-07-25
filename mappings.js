9b08dcf4e49d2bafe3681a57db1ff79cca947c79
Ryan Koval <kovalrya@gmail.com>
2015-07-24 19:13:59 -0500

    rewrote proxy to be more programmatic to allow for more custom actions - uses node-http-proxy again

---
+++ mappings.js
@@ -0,0 +1,21 @@
+var mappings = [{
+  contains: 'blog',
+  redirect: 'http://localhost:2368'
+}, {
+  contains: 'github',
+  redirect: 'https://github.com/rkoval'
+}, {
+  contains: 'resume',
+  redirect: 'https://db.tt/TJzaTvA8'
+}, {
+  contains: 'linkedin',
+  redirect: 'https://www.linkedin.com/pub/ryan-koval/34/44a/459'
+}, {
+  contains: 'twitter',
+  redirect: 'https://twitter.com/whoaitskoval'
+}, {
+  contains: 'dotfiles',
+  redirect: 'https://raw.github.com/rkoval/dotfiles/master/bin/dotfiles'
+}];
+
+module.exports = mappings;
